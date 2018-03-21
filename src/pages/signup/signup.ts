39063b886fb1e777b05b7a997845594775511795
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-03-21 13:13:13 -0300

    Validando se a senha e repetir senha sao iguais

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -19,13 +19,24 @@ export class SignupPage {
     this.formGroup = this.formBuilder.group({
       nome: ['Manuel', [Validators.required, Validators.minLength(5), Validators.maxLength(80)]],
       email: ['manuel@gmail.com', [Validators.required, Validators.email]],
-      senha: ['123456', [Validators.required]],
+      senha: ['123456', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
       repeteSenha: ['123456', [Validators.required]],
+    }, {validator: this.matchingPasswords('senha', 'repeteSenha')});
+  };
 
-    });
+  matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
+    // TODO maybe use this https://github.com/yuyang041060120/ng2-validation#notequalto-1
+    return (group: FormGroup): {[key: string]: any} => {
+      let password = group.controls[passwordKey];
+      let confirmPassword = group.controls[confirmPasswordKey];
+
+      if (password.value !== confirmPassword.value) {
+        return {
+          mismatchedPasswords: true
+        };
+      }
+    }
   }
-
-
 
   signupUser()
   {
