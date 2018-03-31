e41e4cbabf8fd5246822ec6f61c45e6f5f5d85ac
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-03-30 23:43:20 -0300

    Adicionando letra, repeticoes e observacoes na criacao da serie via localStorage

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -2,6 +2,7 @@ import { CartService } from './../../services/domain/cart.service';
 import { CartItem } from './../../models/cart-item';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ExercicioDTO } from '../../models/exercicio.dto';
 
 @IonicPage()
 @Component({
@@ -11,6 +12,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class CartPage {
 
   items: CartItem[];
+  letra: Array<{ letra:string }> = [];
+  repeticao:Array<{ repeticao:string }> = [];
+  observacao:Array<{ observacao:string }> = [];
 
   constructor(
     public navCtrl: NavController, 
@@ -21,6 +25,39 @@ export class CartPage {
   ionViewDidLoad() {
     let cart =  this.cartService.getCart();
     this.items = cart.items
+
+    for(var i=0; i<cart.items.length; i++)
+    {
+      this.observacao[i] = cart.items[i].observacao as any; 
+      this.repeticao[i] = cart.items[i].repeticoes as any; 
+      this.letra[i] = cart.items[i].letra as any; 
+    };
+  };
+
+  removeItem(exercicio: ExercicioDTO)
+  {
+    this.items = this.cartService.removeExercicio(exercicio).items;
+  };
+
+  addLetra(exercicio: ExercicioDTO, index: number)
+  {
+    let letra: any = this.letra[index];
+    
+    this.items = this.cartService.addLetra(exercicio, letra).items;
+  };
+  
+  addRepeticoes(exercicio: ExercicioDTO, index: number)
+  {
+    let repeticao: any = this.repeticao[index];
+    
+    this.items = this.cartService.addRepeticoes(exercicio, repeticao).items;
+  };
+
+  addObservacoes(exercicio: ExercicioDTO, index: number)
+  {
+    let obs:any = this.observacao[index];
+    
+    this.items = this.cartService.addObservacoes(exercicio, obs).items;
   }
 
 }
--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -30,7 +30,7 @@ export class CartService {
         {
             cart.items.push(
                 {
-                    letra: null,
+                    letra: 'A',
                     repeticoes: '10 - 10 - 10',
                     observacao: null,
                     exercicio: exercicio
@@ -39,5 +39,51 @@ export class CartService {
         }
         this.storage.setCart(cart);
         return cart;
-    }
+    };
+
+    removeExercicio(exercicio: ExercicioDTO): Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(index => index.exercicio.id == exercicio.id);
+        if(position != -1)
+        {
+            cart.items.slice(position, 1);
+        }
+        this.storage.setCart(cart);
+        return cart;
+    };
+
+    addLetra(exercicio: ExercicioDTO, letra: string): Cart{
+        let cart = this.getCart();
+        
+        let position = cart.items.findIndex(index => index.exercicio.id == exercicio.id);
+        if(position != -1)
+        {
+            cart.items[position].letra = letra;
+        }
+        this.storage.setCart(cart);
+        return cart;
+    };
+
+    addRepeticoes(exercicio: ExercicioDTO, repeticoes: string): Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(index => index.exercicio.id == exercicio.id);
+        if(position != -1)
+        {
+            cart.items[position].repeticoes = repeticoes;
+        }
+        this.storage.setCart(cart);
+        return cart;
+    };
+
+    addObservacoes(exercicio: ExercicioDTO, observacao: string): Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(index => index.exercicio.id == exercicio.id);
+        if(position != -1)
+        {
+            cart.items[position].observacao = observacao;
+        }
+        this.storage.setCart(cart);
+        return cart;
+    };
+
 }
