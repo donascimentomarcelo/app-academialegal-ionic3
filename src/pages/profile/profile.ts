61190b763c7c04e3440f3043b26a18b561ad7883
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-04-07 19:08:54 -0300

    Habilitando botoes de perfil ao cancelar camera ou galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -88,6 +88,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false; 
     });
   };
 
@@ -104,6 +105,27 @@ export class ProfilePage {
   cancel()
   {
     this.picture = null;
+  };
+
+  getGalleryPicture()
+  {
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+
+    this.picture = 'data:image/png;base64,' + imageData;
+    this.cameraOn = false; 
+    }, (error) => {
+      this.cameraOn = false; 
+    });
   };
 
 };
